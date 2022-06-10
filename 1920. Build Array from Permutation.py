4ea422963dac25e0b359338fa19b99faee5988ad
praveennallapaneni <103142079+praveennallapaneni@users.noreply.github.com>
2022-06-10 15:59:32 -0400

    Add files via upload

---
+++ 1920. Build Array from Permutation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def buildArray(self,nums: List[int]) -> List[int]:
+
+        res = []
+        for i in range(len(nums)):
+            res.append(nums[nums[i]])
+        return res
+
+# space O(N)
+# time O(N)
