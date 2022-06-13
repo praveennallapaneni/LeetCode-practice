c8b2affcc3992c913435f9034150791d9917d6ab
praveennallapaneni <103142079+praveennallapaneni@users.noreply.github.com>
2022-06-13 11:28:24 -0400

    Update and rename 83. Remove Duplicates from Sorted List.txt to 83. Remove Duplicates from Sorted List.py

--- 83. Remove Duplicates from Sorted List.py
+++ 83. Remove Duplicates from Sorted List.py
@@ -1,8 +1,6 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+#Iterative method
+
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
