3ac72fc961b438e22f1bdece9ad95d1fb8ea45e6
Praveen Kumar Nallapaneni <103142079+praveennallapaneni@users.noreply.github.com>
2022-06-21 21:46:21 -0400

    Update 7. Reverse Integer.py

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,24 +1,20 @@
 class Solution:
     def reverse(self, x: int) -> int:     
         res = 0
-        temp = []
         z = abs(x)
         
         while z!=0:
             y = z%10
+            res = res *10 + y
             z = z//10
-            temp.append(y)
-            
+               
         for i in range(len(temp)):
             res = res *10 + temp[i]   
-        
-    
-        if x>0:
-            res = res
-        else: 
-            res =  -res
-            
+       
         if -2**31 <= res <= (2**31)-1:
+            if x>0:
+                res = res
+            else: 
+                res =  -res
             return res
-        else:
-            return 0
+        return 0
