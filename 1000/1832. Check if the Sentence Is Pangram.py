8ad5a1e088a3b6e7e23049f8cceb696f68aa49f9
Praveen Kumar Nallapaneni <103142079+praveennallapaneni@users.noreply.github.com>
2022-06-30 17:16:48 -0400

    Update 1832. Check if the Sentence Is Pangram.py

--- 1000/1832. Check if the Sentence Is Pangram.py
+++ 1000/1832. Check if the Sentence Is Pangram.py
@@ -1,12 +1,7 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        dic = {}
-        for i in sentence:
-            if i not in dic:
-                dic[i]=1
-            else:
-                dic[i]+=1
-                
+        dic = dict(collections.Counter(sentence))
+        
         if len(dic)==26:
             return True
         
