24346289353b316ff357089b94d0e869c699f5c5
Praveen Kumar Nallapaneni <103142079+praveennallapaneni@users.noreply.github.com>
2022-08-19 16:09:56 -0400

    Create 66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> str:
+        
+        digits[-1] += 1
+        
+        for i in range(len(digits)-1, 0, -1):
+            
+            if digits[i] != 10:
+                break
+                
+            digits[i] = 0
+            digits[i-1] += 1
+            
+        if digits[0]==10:
+            digits[0] = 0
+            return [1] + digits
+        
+        return digits
