7e74e38c6e58d6098ff2c7cc18502212faee6bc5
Praveen Kumar Nallapaneni <103142079+praveennallapaneni@users.noreply.github.com>
2022-09-23 16:19:48 -0400

    Update 2089. Find Target Indices After Sorting Array.py

--- 1000/2089. Find Target Indices After Sorting Array.py
+++ 1000/2089. Find Target Indices After Sorting Array.py
@@ -13,3 +13,52 @@ class Solution:
                 r += 1
                       
         return list(range(l, l+r))
+    
+    
+    
+ ## Binary Search Solution
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        
+        nums.sort()
+        
+        def leftIndex(nums,l,r,target):
+            
+            while l<r:
+                mid = l + (r-l)//2
+                
+                if nums[mid] < target:
+                    l = mid +1
+                elif nums[mid-1] < target:
+                    return mid
+                else: 
+                    r = mid-1
+            return l
+        
+        def rightIndex(nums,l,r,target):
+            
+            while l<r:
+                mid  = l + (r-l)//2
+                
+                if nums[mid]> target:
+                    r = mid-1
+                elif nums[mid+1]>target:
+                    return mid
+                else:
+                    l = mid+1
+            return r
+        
+        
+        l,r = 0, len(nums)-1
+        
+        while l<=r:
+            mid = l + (r-l)//2
+            
+            if nums[mid] > target:
+                r = mid-1
+            elif nums[mid] < target:
+                l = mid+1                
+            else:
+                return list(range(leftIndex(nums,0,mid,target),rightIndex(nums,mid, len(nums)-1, target)+1))
+                            
